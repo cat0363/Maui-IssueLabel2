c99d69651c6c2233b67dce2f7c1c9570a2e47869
cat0363 <kenji.nagano.0363@gmail.com>
2023-07-18 16:18:13 +0900

    Repro code of Maui Label Issue on iOS

---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Maui_IssueLabel2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void txtTest_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblTest.Text = txtTest.Text;
+        }
+    }
+}
